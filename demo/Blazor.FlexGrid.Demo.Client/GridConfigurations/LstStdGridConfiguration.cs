9525d03d75bb0098a76abff1ef4bc6e544253cf8
Alex Kallaur <34878069+kalaur@users.noreply.github.com>
2018-07-20 09:15:39 +1000

    Add files via upload

---
+++ demo/Blazor.FlexGrid.Demo.Client/GridConfigurations/LstStdGridConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Blazor.FlexGrid.Components.Configuration;
+using Blazor.FlexGrid.Components.Configuration.Builders;
+using Blazor.FlexGrid.Demo.Shared;
+
+namespace Blazor.FlexGrid.Demo.Client.GridConfigurations
+{
+    public class LstStdGridConfiguration : IEntityTypeConfiguration<LstStd>
+    {
+        public void Configure(EntityTypeBuilder<LstStd> builder)
+        {
+            builder.Property(e => e.odt)
+                .HasCaption("Order date")
+                .HasValueFormatter(d => d.ToShortDateString());
+
+            builder.Property(e => e.fio)
+                .HasCaption("Client Name")
+                .HasOrder(1)
+                .HasValueFormatter(s => $"{s}");
+
+            builder.Property(e => e.csum)
+                .HasCaption("Order Sum")
+                .IsSortable();
+        }
+    }
+}
