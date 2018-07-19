29a442bb1fd72136426baa45abcd928348587904
Alex Kallaur <34878069+kalaur@users.noreply.github.com>
2018-07-20 09:21:49 +1000

    Add files via upload

---
+++ demo/Blazor.FlexGrid.Demo.Shared/LstStd.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blazor.FlexGrid.Demo.Shared
+{
+    public class LstStd
+    {
+        public string cnm { get; set; }
+        public DateTime odt { get; set; }
+        public int osum { get; set; }
+    }
+}
